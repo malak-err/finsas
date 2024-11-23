3dad6c0c23b261c474b982fdc4337e584ab48f31
 <erraoudymalak1997@gmail.com>
2024-11-23 16:51:57 +0100

    add struct

--- finsas.c
+++ finsas.c
@@ -1,4 +1,14 @@
 #include<stdio.h>
+#include<stdlib.h>
+
+typedef struct {
+    char titre[100];
+    char description[200];
+    char date_echeance[20];  
+    char priorite[10];  
+} Tache;
+
+
 
 int main(){
 
